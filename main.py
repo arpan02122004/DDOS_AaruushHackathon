2ae77673eef5d996a0d7b5c49992aaa24ba655d6
Cybergreek021204 <77197355+arpan02122004@users.noreply.github.com>
2022-10-22 23:07:17 +0530

    eddited mistakes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
     while True:
-        raw_data, addr = conn.recvfrom(65536)
+        raw_data, addr = conn.recvfrom(65535)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame')
         print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
@@ -39,7 +39,7 @@ def main():
 
             #TCP
             elif proto == 6:
-                (src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin)
+                (src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]) = tcp_segment(data)
                 print(TAB_1 + 'TCP Segment:')
                 print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(src_port, dest_port))
                 print(TAB_2 + 'Sequence: {}, Acknowledgement: {}'.format(sequence, acknowledgement))
@@ -52,8 +52,9 @@ def main():
             elif proto == 17:
                 src_port, dest_port, length, data = udp_segment(data)
                 print(TAB_1 + 'UDP Segment:')
-                print(TAB_2 + 'Sorce Port: {}, Destination Port: {}, Length: {}'.format(src_port, dest_port))
-
+                print(TAB_2 + 'Sorce Port: {}, Destination Port: {}, Length: {}'.format(src_port, dest_port, length))
+                print(format_multi_line(DATA_TAB_3, data))
+                
             #Other
             else:
                 print(TAB_1 + 'Data:')
@@ -79,7 +80,7 @@ def ipv4_packet(data):
     version_header_length = data[0]
     version = version_header_length >> 4
     header_length = (version_header_length & 15) * 4
-    ttl, proto, src, target = struct.unpack('8x B B 2x 4s 4s', data[:20])
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
     return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
 
 
@@ -90,7 +91,7 @@ def ipv4(addr):
 
 #Unpacks ICMP packet
 def icmp_packets(data):
-    icmp_type, code, checksum = struct.unpack(' ! B B H', data[:4])
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
     return icmp_type, code, checksum, data[4:]
 
 
